7cee5cf93cf48bcb1c992ffd23aa85e49bc2f5a2
dipali sorthiya <dsorathiya77@gmail.com>
2025-09-19 23:12:37 +0530

    java streasms

---
+++ src/main/java/org/example/Streams/StreamIntermediateOperationsExample.java
@@ -0,0 +1,39 @@
+package org.example.Streams;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class StreamIntermediateOperationsExample {
+
+    public static void main(String[] args) {
+
+        List<List<String>> listOfLists = Arrays.asList(
+          Arrays.asList("Reflection","Collection"),
+          Arrays.asList("Structure","State","Flow"),
+          Arrays.asList("Sorting","Mapping","Reduction","Stream")
+        );
+
+        // create a set to hold intermediate results.
+        Set<String> intermediateResults = new HashSet<>();
+
+        // stream pipeline demonstrating various intermediate operations.
+        List<String> result = listOfLists.stream()
+                .flatMap(List::stream)
+                .filter(s -> s.startsWith("S"))
+                .map(String::toUpperCase)
+                .distinct()
+                .sorted()
+                .peek(s -> intermediateResults.add(s))
+                .collect(Collectors.toList());
+
+        System.out.println("Intermediate Results :");
+        intermediateResults.forEach(System.out::println);
+
+        // print the final result.
+        System.out.println("Final Result: ");
+        result.forEach(System.out::println);
+    }
+}
